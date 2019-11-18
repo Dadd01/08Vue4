dd93aebce98175c0831a13f2beb4493921669249
zytong <1832840367@qq.com>
2019-11-18 12:35:25 +0800

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,34 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Index from '../views/Index'
+import Login from '../views/Login'
+import Register from '../views/Register'
+
+Vue.use(VueRouter)
+
+const routes = [
+    {
+      path: '/',
+      name: 'index',
+      component: Index
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: Register
+    }
+
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
